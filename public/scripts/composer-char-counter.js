0f2e54f3d9130eb3c556f8ed941570a270495c32
Luana <luanateixeira148@gmail.com>
2021-07-13 23:47:27 +0000

    set red color to negative countdown

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   console.log('The DOM is ready.');
 
-
   // Tweet count-down
   $( "#tweet-text" ).on("input", function() {
-    const length = $(this).val().length;
-    console.log('length:',length);
+    const $length = $(this).val().length;
     const maxLength = 140;
-    let charactersLeft = maxLength - length;
+    let charactersLeft = maxLength - $length;
+    const $output = $('.counter')
   
-    console.log(charactersLeft);
-  
-    const $output = $('.counter').text(charactersLeft);
+    $output.text(charactersLeft);
+
+    //sets color to red if count is negative
+    if (charactersLeft < 0) {
+      $output.css("color", "#FB1401");
+    } else if (charactersLeft >= 0) {
+      $output.css("color", "#55514A");
+    }
   });
 });
 
