cadcc289aa400d1b43828413c2a8bc62ddf8d278
Luana <luanateixeira148@gmail.com>
2021-07-16 00:36:36 +0000

    refactoring code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,58 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.errorMessage').hide();
+  /* Hides error message by default */
+  $('.error-message').hide();
 
+  /* Makes get request to load old tweets */
   const loadTweets = function () {
-    $.get( '/tweets', function(tweet) {
+    $.get('/tweets', function (tweet) {
       const $tweets = renderTweets(tweet);
       $('.display-tweets').append($tweets);
     });
   };
-
   loadTweets();
 
-  const renderTweets = function(tweets) {
-    // empties the page before rendering all the tweets from the database (so we don't see any duplicate tweets)
-    $('#tweets-container').empty();
-    // loops through tweets
-    for (const tweet of tweets) {
-      // calls createTweetElement for each tweet
-      const $newTweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#tweets-container').append($newTweet);
-    }
-  };
+  /* Tweet Template */
+  const createTweetElement = function (data) {
 
-  // "escapes" the potentially insecure text by re-encoding text.
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  const createTweetElement = function(tweet) {
-    const $tweet = `
+    const $tweetTemplate = `
     <article class="tweet">
       <header>
         <span class="left">
-          <img src=${tweet.user.avatars} alt="user-avatar">
-          <h6 class="name">${tweet.user.name}</h6>
+          <img src=${data.user.avatars} alt="user-avatar">
+          <h6 class="name">${data.user.name}</h6>
         </span>
         <span>
-          <h5 class="username">${tweet.user.handle}</h5>
+          <h5 class="username">${data.user.handle}</h5>
         </span>
       </header>
       <div class="content">
         <p class="tweet">
-        ${escape(tweet.content.text)}
+        ${escape(data.content.text)}
         </p>
       </div>
       <footer>
         <span>
-          <p class="date">${timeago.format(tweet.created_at)}</p> 
+          <p class="date">${timeago.format(data.created_at)}</p> 
         </span>
         <span class="icons">
           <i class="fas fa-flag fa-xs"></i>
@@ -66,40 +50,82 @@ $(document).ready(function() {
     </article>
     `;
 
-    return $tweet;
-    
+    return $tweetTemplate;
+
   };
 
+  /* Renders a new Tweet Element on the Page */
+  const renderTweets = function (tweets) {
 
-  // deals with new tweet form submission
+    $('#tweets-container').empty();
+    for (const tweet of tweets) {
+      const $newTweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($newTweet);
+    }
+
+  };
+
+  /* Deals with new tweet form submission */
   const $form = $('.tweet-form');
-  $form.on('submit', function( event ) {
+  $form.on('submit', function (event) {
     const $tweetText = $('#tweet-text');
-    // console.log($tweetText.val().length)
-
+  
     // serializes the form data as a query string.
     const urlEncodedData = $(this).serialize();
     // stops HTML from submitting form
     event.preventDefault();
-
+  
+    // ensure user is within character limits 
     if ($tweetText.val().length > 140) {
-      $('.errorMessage').slideDown( "slow", ()=>{
-        $('.errorMessage').show();
-      });
+  
+      showError('Your tweet is too long.')
+      return;
+  
     } else if ($tweetText.val().length === 0) {
-      $('.errorMessage').slideDown( "slow", ()=>{
-        $('.errorMessage').show();
-      });
+  
+      showError('Your tweet is empty.')
+      return;
+  
     } else {
-      $('.errorMessage').slideUp( "slow", ()=> {
-        $('.errorMessage').hide();
-      });
-      
+  
+      // $('.error-message').slideUp( "slow", ()=> {
+      $('.error-message').hide();
+      // });
+  
       $.post('/tweets', urlEncodedData, () => {
         loadTweets();
       });
     }
-
+  
   });
 
 });
+
+
+
+/* 
+ * Helper Functions 
+ */
+
+/* Show error message */
+const showError = function (text) {
+
+  $('.error-message').empty().append(`
+      <i class="fas fa-exclamation-circle"></i>
+      <h5>${text}</h5>
+    `).slideDown();
+
+  setTimeout(() => {
+    $('.error-message').empty().slideUp();
+  }, 3000);
+
+}
+
+/* "Escapes" the potentially insecure text by re-encoding input text */
+const escape = function (str) {
+
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+
+};
